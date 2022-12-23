f15cf6cdaaa861efa28e8574881aa8e2527ff8ff
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-12-23 10:11:09 -0700

    add global var for 100vh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App ({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const resizeHandler = () => {
+            const height = `${window.innerHeight}px`
+            document.documentElement.style.setProperty('--vh', height)
+        }
+        window.addEventListener('resize', resizeHandler)
+        return () => {
+            window.removeEventListener('resize', resizeHandler)
+        }
+    }, [])
+
     return <Component {...pageProps} />
 }
