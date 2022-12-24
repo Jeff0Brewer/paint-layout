90713d331b1789a4b7e96eb8ca2525265e7c2f54
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2022-12-23 18:35:42 -0700

    add curved bottom border, remove js vh unit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,7 @@
-import React, { useEffect } from 'react'
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 export default function App ({ Component, pageProps }: AppProps) {
-    useEffect(() => {
-        const resizeHandler = () => {
-            const height = `${window.innerHeight}px`
-            document.documentElement.style.setProperty('--vh', height)
-        }
-        window.addEventListener('resize', resizeHandler)
-        return () => {
-            window.removeEventListener('resize', resizeHandler)
-        }
-    }, [])
-
     return <Component {...pageProps} />
 }
